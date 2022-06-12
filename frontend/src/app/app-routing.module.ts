d15d75d85a67402b7bdd44f163aba26150c4af74
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-06-12 13:01:14 +0000

    Routerの設定追加

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then((c) => c.LoginComponent),
+    },
+    {
+        path: 'top',
+        loadComponent: () => import('./pages/top/top.component').then((c) => c.TopComponent),
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login',
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+    },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes, {
+            preloadingStrategy: PreloadAllModules,
+        }),
+    ],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
